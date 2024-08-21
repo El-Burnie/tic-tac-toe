ddfc68866b5d083a7045b50c2e11776a5adf921c
El-Burnie <173103025+El-Burnie@users.noreply.github.com>
2024-08-20 21:08:54 -0400

    Add basic logic for game board module
    Because there will only be one game board, I decided to use an IIFE to create it.  The game is now playable at the most bare bones level.  I still need to add the ability for the game to check for a win condition.

--- app.js
+++ app.js
@@ -5,32 +5,43 @@ function createCell () {
     const markO = () => value = "O";
     return { getValue, markX, markO };
 };
-/*
-MODULE gameBoard
-    INIT board to []
-    FUNCTION initBoard
-        SET board to []
-        FOR three rows
-            INIT row to []
-            FOR three columns
-                ADD cell() to row
-            ENDFOR
-            ADD row to board
-        ENDFOR
 
-    FUNCTION markCell (row, column, mark)
-        CASE mark OF
-            X: CALL board[row][column].markX()
-            O: CALL board[row][column].markO()
-        ENDCASE
+const gameBoard = (function () {
+    const board = [];
 
-    FUNCTION toString
-        INIT boardString to ""
-        FOR three rows
-            FOR three columns
-                ADD cell value to boardString
-            ENDFOR
-            ADD newline to boardString
-        ENDFOR
-        RETURN boardString
-*/
+    const initBoard = () => {
+        board.splice(0);
+        for (let i = 0; i < 3; i++) {
+            const row = [];
+            for(let j = 0; j < 3; j++) {
+                row.push(createCell());
+            }
+            board.push(row);
+        }
+    };
+
+    const markCell = (row, column, mark) => {
+        switch (mark) {
+            case "X":
+                board[row][column].markX();
+                break;
+            case "O":
+                board[row][column].markO();
+        }
+    };
+
+    const toString = () => {
+        let boardString = "";
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                let cellValue = board[i][j].getValue();
+                boardString += (cellValue) ? `[${cellValue}]` : "[ ]";
+            }
+            boardString += "\n";
+        }
+        return boardString; 
+    };
+
+    return { initBoard, markCell, toString };
+})();
